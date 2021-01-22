e34c7b856ed216959f5eef8ad2f3cb0f9d46677f
zhanzheng8585 <zhanzheng8585@gmail.com>
2021-01-22 00:08:01 -0500

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -14,10 +14,10 @@ def load_init_data(dataname, device, download, dataset_path):
     return train_data, test_data
 
 
-def create_backdoor_data_loader(dataname, train_data, test_data, trigger_label, posioned_portion, batch_size, device):
-    train_data    = PoisonedDataset(train_data, trigger_label, portion=posioned_portion, mode="train", device=device, dataname=dataname)
-    test_data_ori = PoisonedDataset(test_data,  trigger_label, portion=0,                mode="test",  device=device, dataname=dataname)
-    test_data_tri = PoisonedDataset(test_data,  trigger_label, portion=1,                mode="test",  device=device, dataname=dataname)
+def create_backdoor_data_loader(opt, dataname, train_data, test_data, trigger_label, posioned_portion, batch_size, device):
+    train_data    = PoisonedDataset(opt, train_data, trigger_label, portion=posioned_portion, mode="train", device=device, dataname=dataname)
+    test_data_ori = PoisonedDataset(opt, test_data,  trigger_label, portion=0,                mode="test",  device=device, dataname=dataname)
+    test_data_tri = PoisonedDataset(opt, test_data,  trigger_label, portion=1,                mode="test",  device=device, dataname=dataname)
 
     train_data_loader       = DataLoader(dataset=train_data,    batch_size=batch_size, shuffle=True)
     test_data_ori_loader    = DataLoader(dataset=test_data_ori, batch_size=batch_size, shuffle=True)
